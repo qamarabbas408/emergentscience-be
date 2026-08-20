58e68e3e119102201b3324477671e596e38dece7
Qamar Abbas <qmar_abbas110@hotmail.com>
2026-08-20 21:28:00 +0500

    feat: add ?include=topics to discipline-categories journals endpoint

--- app/Http/Controllers/Api/V1/DisciplineCategoryController.php
+++ app/Http/Controllers/Api/V1/DisciplineCategoryController.php
@@ -7,6 +7,7 @@ use App\Http\Resources\V1\DisciplineCategoryResource;
 use App\Http\Resources\V1\JournalResource;
 use App\Models\DisciplineCategory;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class DisciplineCategoryController extends Controller
 {
@@ -30,14 +31,20 @@ class DisciplineCategoryController extends Controller
         return $this->success(new DisciplineCategoryResource($category));
     }
 
-    public function journals(DisciplineCategory $category): JsonResponse
+    public function journals(DisciplineCategory $category, Request $request): JsonResponse
     {
         abort_if(! $category->is_active, 404);
 
-        $journals = $category->journals()
+        $query = $category->journals()
             ->where('is_active', true)
-            ->orderBy('title')
-            ->get();
+            ->orderBy('title');
+
+        $include = array_filter(explode(',', $request->input('include', '')));
+        if (in_array('topics', $include)) {
+            $query->with('topics');
+        }
+
+        $journals = $query->get();
 
         return $this->success(JournalResource::collection($journals));
     }
